7bc2c27c6731af17ed09992419760781623c4a0e
Kenan_E <130177718+Kenan-E@users.noreply.github.com>
2025-05-18 22:45:40 +0400

    I və IV qruplar üçün də countdown
    Bəli, sizi də düşündük. I və IV qruplar da streesdən uzaq qalmamaları üçün və imtahana nə qədər az vaxt qaldığını göstərmək üçün countdown timerimizə əlavə etdik :)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var timer = () => {
-let date = new Date('May 25, 2025 10:00:00 GMT+04:00') - new Date();
+let interval;
+
+var timer2 = () => {
+let date = new Date('May 25, 2025 11:00:00 GMT+04:00') - new Date();
 let days = String(Math.floor(date/(1000*60*60*24))).padStart(2, '0');
 let hours = String(Math.floor(date % (1000*60*60*24) / (1000*60*60))).padStart(2, '0');
 let minutes = String(Math.floor(date % (1000*60*60) / (1000*60))).padStart(2, '0');
@@ -18,5 +20,37 @@ document.getElementById("minutes").textContent = minutes;
 document.getElementById("seconds").textContent = seconds;
 }
 
-const interval=setInterval (timer,1000)
-timer();
+var timer1 = () => {
+let date = new Date('June 1, 2025 11:00:00 GMT+04:00') - new Date();
+let days = String(Math.floor(date/(1000*60*60*24))).padStart(2, '0');
+let hours = String(Math.floor(date % (1000*60*60*24) / (1000*60*60))).padStart(2, '0');
+let minutes = String(Math.floor(date % (1000*60*60) / (1000*60))).padStart(2, '0');
+let seconds = String(Math.floor(date % (1000*60) / (1000))).padStart(2, '0');
+
+if (date<0){
+    days ="🥳";
+    hours = "🎉";
+    minutes = "🎊";
+    seconds = "🪅";
+    clearInterval(interval)
+}
+document.getElementById("days").textContent = days;
+document.getElementById("hours").textContent = hours;
+document.getElementById("minutes").textContent = minutes;
+document.getElementById("seconds").textContent = seconds;
+}
+
+interval=setInterval (timer2,1000)
+timer2();
+
+function btn2() {
+    clearInterval(interval)
+    interval=setInterval (timer2,1000)
+    timer2();
+}
+
+function btn1() {
+    clearInterval(interval)
+    interval=setInterval (timer1,1000)
+    timer1();
+}
